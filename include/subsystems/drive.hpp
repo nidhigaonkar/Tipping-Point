6e3b59ad32ab1db666a8e3f963c2973577a6debd
Cindy Wu <cwu@Cindys-MacBook-Air.local>
2021-08-02 17:25:48 -0700

    test

---
+++ include/subsystems/drive.hpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+using namespace okapi;
+
+//Motor Groups
+//okapi::MotorGroup leftMotorGroup({1, 3});
+//okapi::MotorGroup rightMotorGroup({-2, -4});
+
+//Functions
+void user_drive();
+
+//Controller
+//extern Controller controller;
---
+++ src/subsystems/drive.cpp
@@ -0,0 +1,20 @@
+#include "main.h"
+using namespace okapi;
+
+/*std::shared_ptr<ChassisController> drive =
+  ChassisControllerBuilder()
+    .withMotors(leftMotorGroup, rightMotorGroup)
+    .withDimensions(AbstractMotor::gearset::green, {{4.125, 10}, imev5GreenTPR})
+    .build();*/
+
+
+void user_drive(){
+  while (true)
+  {
+    /*int rightJoystickValue = controller.getAnalog(okapi::ControllerAnalog::rightY);
+    int leftJoystickValue = controller.getAnalog(okapi::ControllerAnalog::leftY);
+    drive -> getModel() -> tank(leftJoystickValue, rightJoystickValue);
+    pros::delay(10);*/
+
+  }
+}
