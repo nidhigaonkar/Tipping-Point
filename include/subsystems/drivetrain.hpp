675a629e1be5c4a1c72c3f7f91bbb27dc4f5c917
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-08 20:09:06 -0700

    Update drivetrain.hpp

--- include/subsystems/drivetrain.hpp
+++ include/subsystems/drivetrain.hpp
@@ -1,12 +1,3 @@
 #include "main.h"
 
-using namespace okapi;
-
-Motor leftBackDrive(2, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor leftFrontDrive(17, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor rightBackDrive(8, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor rightFrontDrive(10, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-
-Controller controller;
-
 void joystickDrive();
