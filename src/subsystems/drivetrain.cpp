e9c819921c7a72a1360f8a0e34691c9b19e42879
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-09 17:55:44 -0700

    Update drivetrain.cpp

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -2,14 +2,17 @@
 
 using namespace okapi;
 
+/*
 std::shared_ptr<ChassisController> drive =
   ChassisControllerBuilder()
-  .withMotors({leftBackDrive, leftFrontDrive}, {rightFrontDrive, rightBackDrive}) 												//MotorGroups for left and right side
-  .withDimensions(AbstractMotor::gearset::blue, {{4_in, 10_in}, imev5BlueTPR})		  //Gearset(rpm) and wheel dimensions
+  .withMotors({leftBackDrive, leftFrontDrive}, {rightFrontDrive, rightBackDrive})   //MotorGroups for left and right side
+  .withDimensions(AbstractMotor::gearset::blue, {{4_in, 10_in}, imev5BlueTPR})		  //Blue gearset(100 rpm) and wheel dimensions
   .build();
 
 void joystickDrive()
 {
-  drive -> getModel() -> arcade(controller.getAnalog(ControllerAnalog::leftY),
-                                controller.getAnalog(ControllerAnalog::rightX));
+  drive -> getModel() -> tank(controller.getAnalog(ControllerAnalog::leftY),
+                              controller.getAnalog(ControllerAnalog::rightY));
+  pros::lcd::set_text(3, "hello");
 }
+**/
