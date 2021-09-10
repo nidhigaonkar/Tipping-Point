269d2a894544b8bf0284f550851f73caafeef4fc
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-09 17:56:34 -0700

    Update globals.cpp

--- src/subsystems/globals.cpp
+++ src/subsystems/globals.cpp
@@ -2,11 +2,14 @@
 
 using namespace okapi;
 
+//Lift Motors
 Motor itz_lift(5, false, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
 
-Motor leftBackDrive(2, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+/*//Drivetrain Motors
+Motor leftBackDrive(6, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
 Motor leftFrontDrive(17, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor rightBackDrive(8, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor rightBackDrive(7, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
 Motor rightFrontDrive(10, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-
+**/
+//Controller
 Controller controller;
