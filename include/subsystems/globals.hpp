420a929a9feff479c48a1a1d1894dee8f0ffe977
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-09 22:52:42 -0700

    Create globals.hpp

---
+++ include/subsystems/globals.hpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+using namespace okapi;
+
+extern Motor leftBackDrive;
+extern Motor leftFrontDrive;
+extern Motor rightBackDrive;
+extern Motor rightFrontDrive;
+
+extern Motor itz_lift;
+
+extern Controller controller;
