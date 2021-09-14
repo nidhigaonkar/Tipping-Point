99335c447a5bfeab4f23652de0f011e2abb6b220
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-13 22:39:44 -0700

    Add files via upload

---
+++ include/subsystems/ports.hpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+//Drivetrain Ports
+const int rightFrontPort = 6;
+const int rightBackPort = 17;
+const int leftFrontPort = 10;
+const int leftBackPort = 7;
+
+//Lift Ports
+const int itzLiftPort = 5;
+const int fourBarLiftPort = 0;
+
+//Roller intake Port
+const int rollerPort = 0;
