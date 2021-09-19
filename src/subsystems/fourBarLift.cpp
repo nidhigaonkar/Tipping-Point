9e46d56907720527ff19eb03cedbe1babd1958dc
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-19 15:46:17 -0700

    Update fourBarLift.cpp

--- src/subsystems/fourBarLift.cpp
+++ src/subsystems/fourBarLift.cpp
@@ -4,40 +4,38 @@ using namespace okapi;
 
 Motor fourBarLift(fourBarLiftPort, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
 
-double setpoint = 0;
-double integral = 0;
-double prevError;
-double kP;
-double kI;
-double kD;
-double target;
-double error;
-double derivative;
-double power;
+double setpoint;
+
+typedef struct PID pid;
+
+pid FB;
+
+double PIDFourBar(double setpoint)
+{
+  FB.kP = 0.5;
+  FB.kI = 0;
+  FB.kD = 0.1;
+
+  FB.target = setpoint;
+  FB.error = FB.target - fourBarLift.getPosition();
+  FB.integral += FB.error;
+  FB.derivative = FB.error - FB.prevError;
+
+  FB.power = FB.kP * FB.error + FB.kI * FB.integral + FB.kD * FB.derivative;
+
+  return FB.power;
+}
+
 
 void updateFourBarLift()
 {
-  fourBarLift.moveVelocity(100 * (controller.getDigital(ControllerDigital::L1) - controller.getDigital(ControllerDigital::L2)));
-
-  if (controller.getDigital(ControllerDigital::L1) == 0 && controller.getDigital(ControllerDigital::L2) == 0)
+  if (controller.getDigital(ControllerDigital::R1) == 0 && controller.getDigital(ControllerDigital::R2) == 0)
   {
     setpoint = fourBarLift.getPosition();
     fourBarLift.moveVelocity(PIDFourBar(setpoint));
   }
-}
-
-double PIDFourBar(double setpoint)
-{
-  kP = 0.5;
-  kI = 0;
-  kD = 0.1;
-
-  target = setpoint;
-  error = setpoint - fourBarLift.getPosition();
-  integral += error;
-  derivative = error - prevError;
-
-  power = kP * error + kI * integral + kD * derivative;
-
-  return power;
+  else
+  {
+    fourBarLift.moveVelocity(100 * (controller.getDigital(ControllerDigital::R1) - controller.getDigital(ControllerDigital::R2)));
+  }
 }
