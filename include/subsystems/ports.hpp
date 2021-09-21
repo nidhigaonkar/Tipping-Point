c6315de3c7f78f146bb81f325c7d8d30549d00e1
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-09-20 17:04:24 -0700

    Update ports.hpp

--- include/subsystems/ports.hpp
+++ include/subsystems/ports.hpp
@@ -4,14 +4,14 @@
 const int rightFrontPort = 6;
 const int rightBackPort = 17;
 const int leftFrontPort = 10;
-const int leftBackPort = 7;
+const int leftBackPort = 12;
 
 //Lift Ports
 const int itzLiftPort = 5;
 const int fourBarLiftPort = 11;
 
 //Roller intake Port
-const int rollerPort = 0;
+const int rollerPort = 14;
 
 //Pneumatic Ports
 
