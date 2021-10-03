10653b73a08e3bb70d778606e6b78f42a9a920c8
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-10-02 22:01:24 -0700

    Update pneumatics.cpp

--- src/subsystems/pneumatics.cpp
+++ src/subsystems/pneumatics.cpp
@@ -2,12 +2,12 @@
 
 void updatePneumatics()
 {
-   if (controller.getDigital(ControllerDigital::A) == 1)
-   {
-     pros::c::adi_digital_write(pneumaticLeftPort, HIGH);
-   }
-   else if (controller.getDigital(ControllerDigital::B) == 1)
+   if (controller.getDigital(ControllerDigital::B) == 1)
    {
      pros::c::adi_digital_write(pneumaticLeftPort, LOW);
+   }
+   else
+   {
+     pros::c::adi_digital_write(pneumaticLeftPort, HIGH);
    }
 }
