91718abeb047400b2927674c6e5b84111bc48162
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-10-02 22:02:04 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ void initialize() {
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::c::adi_pin_mode(pneumaticLeftPort, OUTPUT);
-	pros::c::adi_pin_mode(pneumaticRightPort, OUTPUT);
+	//pros::c::adi_pin_mode(pneumaticRightPort, OUTPUT);
 
 	pros::lcd::register_btn1_cb(on_center_button);
 }
@@ -63,7 +63,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	awpAuton();
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -80,14 +82,18 @@ void autonomous() {}
  */
 void opcontrol()
 {
+	awpAuton();
 	while(true)
 	{
 		updateDrive();
-		updateItzLift();
-		updateFourBarLift();
+		updateItzMacro();
+		//updateItzLift();
+		//updateFourBarLift();
+		updateFourMacro();
 		updateRollers();
 		updatePneumatics();
 
 		pros::delay(10);
 	}
+
 }
