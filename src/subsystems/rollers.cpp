2d065a9acf28e25c75a8527d9d9efae35ff019a1
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-10-02 22:01:48 -0700

    Update rollers.cpp

--- src/subsystems/rollers.cpp
+++ src/subsystems/rollers.cpp
@@ -6,6 +6,6 @@ Motor rollers(rollerPort, false, AbstractMotor::gearset::green, AbstractMotor::e
 
 void updateRollers()
 {
-  rollers.moveVelocity(200 * (controller.getDigital(ControllerDigital::R1) - controller.getDigital(ControllerDigital::R2)));
+  rollers.moveVelocity(100 * (controller.getDigital(ControllerDigital::R1) - controller.getDigital(ControllerDigital::R2)));
   rollers.setBrakeMode(AbstractMotor::brakeMode::coast);
 }
