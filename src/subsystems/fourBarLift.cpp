59f3167a7f3f1685c84e524fd77a179071d67a4e
Lawrence_3067 <59271101+lawrence3067@users.noreply.github.com>
2021-10-02 21:57:47 -0700

    Update fourBarLift.cpp

--- src/subsystems/fourBarLift.cpp
+++ src/subsystems/fourBarLift.cpp
@@ -5,6 +5,7 @@ using namespace okapi;
 Motor fourBarLift(fourBarLiftPort, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
 
 double setpoint;
+int liftButtonCount;
 
 typedef struct PID pid;
 
@@ -14,7 +15,7 @@ double PIDFourBar(double setpoint)
 {
   FB.kP = 0.5;
   FB.kI = 0;
-  FB.kD = 0.1;
+  FB.kD = 0.2;
 
   FB.target = setpoint;
   FB.error = FB.target - fourBarLift.getPosition();
@@ -37,5 +38,38 @@ void updateFourBarLift()
   else
   {
     fourBarLift.moveVelocity(100 * (controller.getDigital(ControllerDigital::L1) - controller.getDigital(ControllerDigital::L2)));
+  }
+}
+
+
+void updateFourMacro()
+{
+  if (controller.getDigital(ControllerDigital::L1) == 1)
+  {
+    liftButtonCount = 3;
+  }
+
+  else if(controller.getDigital(ControllerDigital::L2) == 1)
+  {
+
+    liftButtonCount = 1;
+    if(liftButtonCount > 1){
+      liftButtonCount = 1;
+    }
+  }
+
+
+
+  if (liftButtonCount == 3)
+  {
+    fourBarLift.moveVelocity(PIDFourBar(800)); //highest position
+  }
+  else if(liftButtonCount == 2){
+    fourBarLift.moveVelocity(PIDFourBar(100));
+  }
+  else if (liftButtonCount == 1)
+  {
+    fourBarLift.moveVelocity(PIDFourBar(0));//was 0
+    liftButtonCount+=1;
   }
 }
